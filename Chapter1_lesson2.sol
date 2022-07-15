9db85ef6250f4091517b5d8a6ae1a25ac1706018
Gayan <1513223865@qq.com>
2022-07-15 15:34:53 +0800

    Create Chapter1_lesson2.sol

---
+++ Chapter1_lesson2.sol
@@ -0,0 +1,9 @@
+// A contract is the fundamental building block of Ethereum applications — all variables and functions belong to a contract
+
+pragma solidity >=0.5.0 <0.6.0; // Solidity compiler versions 
+
+contract ZombieFactory {
+
+    //start here
+
+}
